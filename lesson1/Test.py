e2a7f1b3800100a0007d6d81e0b7921978144900
abramova-t-testers <abramova.t.testers@gmail.com>
2025-12-02 23:16:22 +0300

    lesson1

---
+++ lesson1/Test.py
@@ -0,0 +1,25 @@
+my_heigh = 166 # Создайте переменную
+print (my_heigh)
+
+my_name = 'Татьяна' # Перезапишите переменную
+my_name = 'Татьяна Абрамова'
+print (my_name)
+
+pet_name = input("Как зовут вашего питомца? ") # Получите пользовательский ввод
+print("Ваш любимчик - " + pet_name)
+
+def print_python():
+    print("Учу Python!") # Создание функции
+
+print_python()
+
+def print_letter(let):
+    print(let, end='') # Параметризация функций
+
+print_letter('С')
+print_letter('т')
+print_letter('у')
+print_letter('д')
+print_letter('е')
+print_letter('н')
+print_letter('т')
